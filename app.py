9d8f7816f0b064708f6f82f9a21078c81992ecd6
kenrick campbell <kennyg8810@gmail.com>
2022-12-22 10:59:52 -0500

    added ability to download audio

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 import streamlit as st
 from pytube import YouTube 
 
+def first(iterable, default=None):
+  for item in iterable:
+    return item
+  return default
+
 with st.sidebar:
     # get youtube url
     url = st.text_input("YouTube URL")
     quality_options = []
     if url:
         yt = YouTube(url)
+        # get the audio from the video
         audio_list = yt.streams.filter(only_audio=True)
-        for audio in audio_list:
-            quality_options.append(audio.abr)
+        for i in audio_list:
+            # put aviable quality options into a list
+            quality_options.append(i.abr)
         print(quality_options)
 
 with st.container():
     col1, col2 = st.columns(2)
-    
 
     with col1:
-        st.image(yt.thumbnail_url, width=128)
+        if url:
+            st.image(yt.thumbnail_url, width=128)
     with col2:
-        option = st.selectbox(
-            "Choose the audio quality you are looking for",
-            (quality_options)
+        if url:
+            option = st.selectbox(
+                "Choose the audio quality you are looking for",
+                (quality_options)
+                )
+            st.write("you selected: ", option)
+            stream = first(x for x in audio_list if x.abr == option)
+            print(type(stream))
+            audio = yt.streams.get_by_itag(stream.itag)
+            btn = st.download_button(
+                label='Download audio file',
+                data=audio.download(),
+                mime=stream.mime_type
             )
-        st.write("you selected: ", option)
